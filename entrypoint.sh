70d1d07b4a6085310d9cfc5c1b5ccad5fff13835
javila35 <josepharthuravila@gmail.com>
2021-06-11 23:59:38 -0700

    Commit made by prettier

---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# e: Fails immediately on an error
+# u: Missing values are treated as errors
+set -eu
+
+_main() {
+		_switch_to_branch
+
+		_add_files
+
+		_local_commit
+
+		_push_to_github
+}
+
+_switch_to_branch() {
+	echo "Checking out new branch";
+	git checkout -b ${{ github.head_ref }}
+}
+
+_add_files() {
+	echo "Adding files";
+	# # shellcheck disable=SC2086
+    git add .
+}
+
+_local_commit() {
+    git commit -m "Commit made by prettier";
+}
+
+
+_push_to_github() {
+	echo "push to github";
+	# "git push -u origin ${{ github.head_ref }}"
+}
+
+_add_files
---
+++ progress.md
@@ -0,0 +1,3 @@
+Currently reverse engineering the auto commit action: https://github.com/stefanzweifel/git-auto-commit-action
+
+Reading along with Michael Heap's article: https://michaelheap.com/git-auto-commit/
